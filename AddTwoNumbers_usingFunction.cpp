5fac64e22131dc1fadc5f0af161ca1a0a4bc8cc4
R <sujairaj.r@aptiv.com>
2026-03-09 15:30:05 +0530

    added changes for subraction

--- AddTwoNumbers_usingFunction.cpp
+++ AddTwoNumbers_usingFunction.cpp
@@ -13,5 +13,9 @@ int main()
     std::cin >> a >> b;
     int res = addition(a, b);
     std::cout << "addition result is :" << res << std::endl;
+
+    int c = 5, d = 3;
+    int res1 = c - d;
+    std::cout << "subraction result is :" << res1 << endl;
     return 0;
 }
